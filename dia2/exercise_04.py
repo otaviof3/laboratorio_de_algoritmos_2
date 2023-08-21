cc7bb76d0b632559273dc6d434cf77a8336504b4
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-08-21 20:30:14 -0300

    Create exercise_04.py

---
+++ dia2/exercise_04.py
@@ -0,0 +1,18 @@
+def add(matriz):
+    average = 0
+    for line in range(len(matriz)):
+        line_average = 0
+        for column in range (len(matriz[line])):
+            line_average = line_average + matriz[line][column]
+            average = average + matriz[line][column]
+        print(f"Line {(matriz[line])}:",line_average / 3)
+    return average
+def main():
+    matriz = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+    average = add(matriz)
+    print("Total average:",average / 9)
+main()
