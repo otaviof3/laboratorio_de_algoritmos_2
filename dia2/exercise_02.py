110072129fc2c9ae6501b3571fc22f5e29ba1e7b
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-08-23 13:48:33 -0300

    Add files via upload

---
+++ dia2/exercise_02.py
@@ -0,0 +1,15 @@
+def main():
+    list = [1,1,1,2,3,4,4]
+    unified_list,duplicates = unify_list(list)
+    print(unified_list)
+    print("Duplicates:",duplicates)
+def unify_list(list):
+    unified_list = []
+    duplicates = []
+    for i in range(0,len(list)):
+        if list[i] not in unified_list:
+            unified_list.append(list[i])
+        else:
+            duplicates.append(list[i])
+    return unified_list,duplicates
+main()
