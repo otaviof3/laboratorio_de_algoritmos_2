b7deef8266fc8ff367a5ec86bed51d4da423eef4
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-08 11:29:09 -0300

    Add files via upload

---
+++ aula8/exercise2.py
@@ -0,0 +1,14 @@
+def control_f():
+    my_file = open("words.txt", "r")
+    words = my_file.readlines()
+    line = 1
+    result = input("Word to find: ")
+    for word in words:
+        if word.find(result) != -1:
+            print(f"Word {result} found in line > {line}")
+            line += 1
+        else:
+            line += 1
+def main():
+    control_f()
+main()
