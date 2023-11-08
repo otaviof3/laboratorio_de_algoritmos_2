95ce47b8c2fbb8e12c09849dbc8157ac6110ea89
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-08 19:46:19 -0300

    Update exercise3.py

--- aula8/exercise3.py
+++ aula8/exercise3.py
@@ -6,6 +6,8 @@ def copy_file():
         copy_name = "copia_" + input_filename
         copied_file = open(copy_name, "w")
         copied_file.write(file_lines)
+        my_file.close()
+        copied_file.close()
     except FileNotFoundError:
         print("File not found.")
 def main():
